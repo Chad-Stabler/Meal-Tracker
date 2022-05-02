9eefa7edb8b2658f949d4933ab48ed49a6d105ef
Chad Stabler <stablerpsn@gmail.com>
2022-05-02 16:46:59 -0500

    grabbing ingredient data from form and updating to array

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 // import functions and grab DOM elements
+const ingredientForm = document.getElementById('ingredient-form');
 
 // let state
-
+let ingredients = [];
 // set event listeners 
+ingredientForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const ingredientData = new FormData(ingredientForm);
+    let ingredient = {
+        ingredient: ingredientData.get('ingredient'),
+        quantity: ingredientData.get('quantity'),
+        measurement: ingredientData.get('measurement'),
+    };
+    ingredients.push(ingredient);
+    ingredientForm.reset();
+});
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
